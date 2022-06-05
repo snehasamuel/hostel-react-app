0dab6940099a86b5a13df68cee47b0cfb15815d8
snehasamuel <snehasamuel297@gmail.com>
2022-06-05 11:30:06 +0530

    Development created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Search from './components/Search';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" exact element={<Login/>}/>
+      <Route path="/register" exact element={<Register/>}/> 
+      <Route path="/search" exact element={<Search/>}/>
+      <Route path="/view" exact element={<View/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
