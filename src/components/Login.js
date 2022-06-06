239217b1c3a0e2c27855fe3307ed2413b3c43b68
snehasamuel <snehasamuel297@gmail.com>
2022-06-06 19:10:54 +0530

    Login completed

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,7 +17,11 @@ const Login = () => {
                     <input type="password" name="" id="" className="form-control"/>
                 </div>
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <button className="btn btn-primary">LOGIN</button><br></br><br></br>
+                <Link to="/search">
+                    <button className="btn btn-primary">
+                      LOGIN
+                      </button>
+                      </Link><br></br><br></br>
                     <Link to="/register">New User ?? Click Here !!</Link>
                 </div>
             </div>
